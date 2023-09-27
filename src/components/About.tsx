41b2e3b6f919b985eceb5c088183bfad9066cab3
Your Name <mdhmaktaruzzaman9101@gmail.com>
2023-09-27 23:30:42 +0600

    typescript

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,18 +1,42 @@
 import React, { useState } from 'react';
 
-const About = (props: {title: string}) => {
+type UserProps = {
+    val:{
+    title: string,
+    num:number,
+    isArray: number[],
+  };
+}
+
+const About = ({val }: UserProps) => {
     const [count, setCount] = useState<number>(0);
 
     const handleCount = () =>{
         setCount(count + 1)
     }
-  return (
+
+return (
     <>
-        <div>{props.title}</div>
+
+        {/* <div>{title} and {num}</div>
+        <div>This is array = {isArray}
+    
+        </div>
+        <h1>Map array</h1>
+        <p>
+            {isArray.map((item,index)=>{
+                return <span>{item}{index}</span>
+            })}
+        </p> */}
+
+        <h1>{val.title}</h1>
+        <h1>{val.num}</h1>
+        <h1>{val.isArray}</h1>
         <h1>{count}</h1>
         <p onClick={handleCount}>Click</p>
+        
     </>
-  )
+)
 }
 
 export default About
